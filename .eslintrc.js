d891aa8cc603d9921d34f0a4380873d9cada7376
sergio.touset <sergiotl@ext.bershka.com>
2022-07-15 13:11:57 +0200

    feat: add Eslint + prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ],
+  settings: {
+    react: { version: 'detect' }
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'space-before-function-paren': 'off',
+    'react/prop-types': 'off',
+    'no-use-before-define': 'off',
+    'react/react-in-jsx-scope': 'off'
+  }
+}
