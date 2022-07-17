82ac72858ca2a40774f0518fd225f4126af3e5b9
sergio.touset <sergiotl@ext.bershka.com>
2022-07-17 11:00:34 +0200

    feat: refactor film-card.jsx so it shows properly the date

--- components/film-card/film-card.jsx
+++ components/film-card/film-card.jsx
@@ -1,14 +1,18 @@
 import Link from 'next/link'
+import { useMemo } from 'react'
+import { parse, formatDistanceStrict } from 'date-fns'
 
 export default function FilmCard({ id, title, releaseDate, episode }) {
+  const formattedDate = useMemo(() => {
+    const date = parse(releaseDate, 'yyyy-MM-dd', new Date())
+    return `${releaseDate} (${formatDistanceStrict(date, new Date())} ago)`
+  }, [releaseDate])
   return (
     <Link href={`/film/${id}`}>
       <div className="flex flex-col w-full sm:w-5/12 p-8 border rounded-xl mt-4 sm:even:ml-4">
-        <div className="flex">
-          <div>Episode {episode}</div>
-          <div>{title}</div>
-        </div>
-        <div className="self-end text-xs">{releaseDate}</div>
+        <div>{title}</div>
+        <div>Episode {episode}</div>
+        <div className="self-end text-xs">{formattedDate}</div>
       </div>
     </Link>
   )
